2f0da259978eb0090cc1af1af08d95a1dec5b2b8
Mike Hadlow <mike.hadlow@15below.com>
2012-11-23 15:51:48 +0000

    ProducerConsumer

---
+++ Mike.Spikes/Puzzlers/FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mike.Spikes.Puzzlers
+{
+    public class FizzBuzz
+    {
+        public void Linq()
+        {
+            var result =
+                from n in HandyFunctions.NaturnalNumbers().Take(15)
+                select
+                    n % 15 == 0 ? "FizzBuzz" :
+                    n % 3 == 0 ? "Fizz" :
+                    n % 5 == 0 ? "Buzz" :
+                    n.ToString();
+
+            Console.Out.WriteLine(string.Join(", ", result));
+        }     
+    }
+
+    public static class HandyFunctions
+    {
+        // I don't like enumerable range
+        public static IEnumerable<int> NaturnalNumbers()
+        {
+            var n = 0;
+            while (true)
+            {
+                yield return ++n;
+            }
+        }
+    }
+}
