c4f074d3b27126943477ce475270a83b0b7343b6
Mike Hadlow <mike@suteki.co.uk>
2013-03-05 09:41:31 +0000

    Vowels

---
+++ Mike.Spikes/Puzzlers/Vowels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Mike.Spikes.Puzzlers
+{
+    public class Vowels
+    {
+        private const string path = @"D:\Temp\Words\Words.txt";
+
+        public void FirstAttempt()
+        {
+            var words = File.ReadAllLines(path).Where(word => word.Intersect("aeiou").Count() == 5);
+            foreach (var word in words)
+            {
+                Console.Out.WriteLine("word = {0}", word);
+            }
+        }
+    }
+}
